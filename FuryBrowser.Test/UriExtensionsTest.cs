ffc6ed53d2263037c480629c86cf14f5d8864c0d
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-11-14 21:22:15 -0500

    tdd: UpgradeToHttps_WithQueryAndFragment

--- FuryBrowser.Test/UriExtensionsTest.cs
+++ FuryBrowser.Test/UriExtensionsTest.cs
@@ -25,4 +25,14 @@ public class UriExtensionsTest
 
 		Assert.AreEqual("https://duckduckgo.com/", actual.ToString());
 	}
+
+	[TestMethod]
+	public void UpgradeToHttps_WithQueryAndFragment()
+	{
+		var input = new Uri("https://learn.microsoft.com/en-us/dotnet/api/system.uribuilder.-ctor?f1url=%3FappId%3DDev16IDEF1%26l%3DEN-US%26k%3Dk(System.UriBuilder.%2523ctor)%3Bk(DevLang-csharp)%26rd%3Dtrue&view=net-7.0#system-uribuilder-ctor(system-string-system-string)");
+
+		var actual = UriExtensions.UpgradeToHttps(input);
+
+		Assert.AreEqual(input.ToString(), actual.ToString());
+	}
 }
--- FuryBrowser/UriExtensions.cs
+++ FuryBrowser/UriExtensions.cs
@@ -6,7 +6,9 @@ public static class UriExtensions
 {
 	public static Uri UpgradeToHttps(this Uri source)
 	{
-		var ub = new UriBuilder("https://", source.Host, -1, source.PathAndQuery);
+		var ub = new UriBuilder("https://", source.Host, -1, source.AbsolutePath);
+		ub.Query = source.Query;
+		ub.Fragment = source.Fragment;
 		return ub.Uri;
 	}
 }
