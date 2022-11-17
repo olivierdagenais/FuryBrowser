7e74bb55b5e507dfd4a9001a128f436f66564254
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-11-16 21:27:16 -0500

    refactor: rename MainWindow -> MainView

--- FuryBrowser/MainView.xaml.cs
+++ FuryBrowser/MainView.xaml.cs
@@ -8,11 +8,11 @@ using Microsoft.Web.WebView2.Core;
 namespace FuryBrowser;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+/// Interaction logic for MainView.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainView : Window
 {
-	public MainWindow()
+	public MainView()
 	{
 		// TODO: read from the registry and/or settings
 		TextOptions.SetTextFormattingMode(this, TextFormattingMode.Display);
